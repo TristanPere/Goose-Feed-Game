b7bd5fd72827f71349082b06f2b9a0670a66ad92
TristanP <tristanpere@googlemail.com>
2023-01-27 16:34:15 +0000

    Point and click game that loads geese and unloads them on clicks works

--- main.js
+++ main.js
@@ -20,18 +20,30 @@ const hatchGeese = (idArr) => {
 };
 
 const handleRestart = () => {
+  gallery.innerHTML = "";
   hatchGeese(idArr);
-  const fedCount = 0;
+  let fedCount = 0;
   const gooseArr = document.querySelectorAll(".goose");
   const gooseButtonArr = document.querySelectorAll(".goose__button");
-  //   console.log(gooseArr);
 
-  //   const positionGoose = () => {
-  //     gooseArr.style.top = 90 + "px";
-  //     gooseArr.style.left = 90 + "px";
-  //   };
-  //   positionGoose();
-
+  const positionArrGenerator = (length) => {
+    let arrayXY = [];
+    let x = [];
+    for (let i = 0; i < length; i++) {
+      x[0] = Math.random() * (gallery.clientWidth-75); //width
+      x[1] = Math.random() * (gallery.clientHeight - 75); //height
+      arrayXY[i] = [Math.floor(x[0]), Math.floor(x[1])];
+    }
+    return arrayXY;
+  };
+  const positionGeese = () => {
+    const positionArr = positionArrGenerator(idArr.length);
+    for (let i = 0; i < gooseArr.length; i++) {
+      gooseArr[i].style.left = positionArr[i][0] + "px";
+      gooseArr[i].style.top = positionArr[i][1] + "px";
+    }
+  };
+  positionGeese();
   const handleFeeding = (event) => {
     gooseArr[event.target.value].remove(); //changing galleryDiv but not changing gooseArry
     fedCount += 1;
