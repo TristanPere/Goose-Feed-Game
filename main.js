5f5a9d7831dc351b1d7c18177ac483e9c26d4ced
TristanP <tristanpere@googlemail.com>
2023-01-27 15:39:04 +0000

    Can load multiple geese to display then remove one dependent on what is clicked

--- main.js
+++ main.js
@@ -2,49 +2,43 @@ const gallery = document.querySelector(".gallery");
 const restartButton = document.querySelector(".title-bar__re-start");
 
 const gooseHTML = (idNum) => {
-    const innerHTML = `<div class="goose" id="goose${idNum}">
-        <button class="goose__button" value="goose${idNum}">
-            <img class="goose__image" src="goose.png" alt="goose" />
-        </button>
+  const innerHTML = `<div class="goose" id="goose${idNum}">
+    <input class="goose__button" type="image" src="goose.png" value="${idNum}">
     </div>`;
-    return innerHTML;
+  return innerHTML;
 };
-const hatchGoose = () =>{
-    gallery.innerHTML = gooseHTML(0)
-    return gallery.innerHTML
-}
-const idArr = [0,1,2,3,4,5,6,7,8,9]
+const hatchGoose = () => {
+  gallery.innerHTML = gooseHTML(0);
+  return gallery.innerHTML;
+};
+const idArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const hatchGeese = (idArr) => {
-    for (let i=0; i<idArr.length;i++){
-        gallery.innerHTML += gooseHTML(idArr[i])
-    }
-}
-
+  for (let i = 0; i < idArr.length; i++) {
+    gallery.innerHTML += gooseHTML(idArr[i]);
+  }
+};
 
 const handleRestart = () => {
-    hatchGeese(idArr)
-
+  hatchGeese(idArr);
+  const fedCount = 0;
   const gooseArr = document.querySelectorAll(".goose");
   const gooseButtonArr = document.querySelectorAll(".goose__button");
-//   console.log(gooseArr);
+  //   console.log(gooseArr);
 
-//   const positionGoose = () => {
-//     gooseArr.style.top = 90 + "px";
-//     gooseArr.style.left = 90 + "px";
-//   };
-//   positionGoose();
+  //   const positionGoose = () => {
+  //     gooseArr.style.top = 90 + "px";
+  //     gooseArr.style.left = 90 + "px";
+  //   };
+  //   positionGoose();
 
   const handleFeeding = (event) => {
-    // document.getElementById("goose${idNum}").remove();
-    // gooseArr[event.target.value].remove()
-    console.log(event.target)
-// console.log( gooseArr)
+    gooseArr[event.target.value].remove(); //changing galleryDiv but not changing gooseArry
+    fedCount += 1;
   };
-  gooseButtonArr.forEach((gooseButton)=>{
+  gooseButtonArr.forEach((gooseButton) => {
     gooseButton.addEventListener("click", handleFeeding);
-  })
-  
+  });
 };
 
 restartButton.addEventListener("click", handleRestart);
