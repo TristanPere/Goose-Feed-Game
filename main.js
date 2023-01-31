cdf4ad674ccf1c28855c6eaeb3a30636e571addd
TristanP <tristanpere@googlemail.com>
2023-01-31 19:44:35 +0000

    Completion of working game with Level Select div.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const gallery = document.querySelector(".gallery");
 const restartButton = document.querySelector(".title-bar__re-start");
 const score = document.querySelector(".title-bar__score");
 const levelSelect = document.querySelector(".level-select");
-const roundTimer = document.querySelector(".round-timer");
+const roundTimer = document.querySelector("#round-timer");
+const currentRound = document.querySelector("#current-round");
+let highestAvailableRounds = 1;
 
 const LevelSelectHTML = (idNum) => {
   const innerHTML = `<div class="level" id="level${idNum}">
@@ -61,45 +63,50 @@ const positionGeese = (gooseArr) => {
 };
 
 const countDownTimer = (amountOfTime) => {
-  let sec = amountOfTime/1000;
+  let sec = Math.floor(amountOfTime / 1000);
   let timer = setInterval(() => {
-    roundTimer.innerHTML = `00:${sec} seconds left`;
-    sec--; 
+    roundTimer.innerHTML = `Round Timer: 00:${sec}s`;
+    sec--;
   }, 1000);
-  setTimeout(()=>{clearInterval(timer)}, amountOfTime+1000);
+  setTimeout(() => {
+    clearInterval(timer);
+  }, amountOfTime + 1000);
 };
 
 // all functions to occur in a round are store in here so can be called when a round button is clicked
-let highestAvailableRounds = 1;
 const handleRound = (event) => {
   // packaged handleRestart into handle a round with input of numberOfGeese and round number to determine speed of geese appearing
   if (event.target.value <= highestAvailableRounds) {
     const roundNumber = event.target.value;
-    const numberOfGeese = 10 * (1.3 * roundNumber);
-    // console.log(roundNumber)
+    const numberOfGeese = Math.floor(10 * (1.5 * roundNumber));
+    const idArr = [];
+    for (let i = 0; i < numberOfGeese; i++) {
+      idArr.push(i);
+    }
+    const spawnSpeed = (1000 * 1) / (0.5 * roundNumber);
+    const lifeTime = 3 * spawnSpeed;
+    const roundLength = spawnSpeed * numberOfGeese + lifeTime;
     let fedCount = 0;
-    // const numberOfGeese = 20;
-    const idArr = [...Array(numberOfGeese).keys()]; // idArr is the number of geese on screen in a round
     gallery.innerHTML = "";
+    currentRound.innerHTML = `Current Round: ${event.target.value}`;
     hatchGeese(idArr);
 
     const gooseArr = document.querySelectorAll(".goose");
     const gooseButtonArr = document.querySelectorAll(".goose__button");
     let hatchedGeese = 0;
     let lostGeese = 0;
-    // moves the geese on screen into place
     positionGeese(gooseArr);
 
+    // moves the geese on screen into place
+    countDownTimer(roundLength);
     const handleFeeding = (event) => {
-      gooseArr[event.target.value].style.display = "none"; //no longer changing galleryDiv. instead just disappearing the geese
+      gooseArr[event.target.value].style.display = "none";
       fedCount += 1;
       score.innerText = `Geese Fed:${fedCount}/${numberOfGeese}`;
     };
-
     gooseButtonArr.forEach((gooseButton) => {
       gooseButton.addEventListener("click", handleFeeding);
     });
-  
 
     //This function:
     //Adds geese in order of their id
@@ -125,43 +132,45 @@ const handleRound = (event) => {
       }
     };
 
-    const spawnSpeed = (1000 * 1) / (0.5 * roundNumber);
-    const lifeTime = 3 * spawnSpeed;
-    const roundLength = spawnSpeed * numberOfGeese + lifeTime;
-    countDownTimer(roundLength)
     gooseHatchInterval = setInterval(gooseHatchTimed, spawnSpeed);
     setTimeout(() => {
       gooseLooseInterval = setInterval(gooseLooseTimed, spawnSpeed);
     }, lifeTime); // Adds lifetime delay onto despawning of each goose
-
-    
 
     // After a round is complete it checks if number of geese fed are greater than 50% if so next level is unlocked.
     setTimeout(() => {
       if (fedCount >= numberOfGeese / 2) {
         // levelSelectButtons[event.target.value].addEventListener("click", handleRound);
         levelButtonsHTML[event.target.value].style.backgroundColor = "white";
-        highestAvailableRounds +=1
+        highestAvailableRounds += 1;
       }
     }, roundLength);
   } else {
-    alert(`Must feed 50% of geese from level ${event.target.value - 1}`);
+    alert(
+      `Must feed 50% of geese from level ${
+        event.target.value - 1
+      } before proceeding to level ${event.target.value}`
+    );
   }
 };
 
-const handleRestart = () => {
-  highestAvailableRounds = 1;
-  levelButtonsHTML.forEach((buttonHTML) => {
-    buttonHTML.style.backgroundColor = "rgb(58, 58, 58)";
-  });
-  levelButtonsHTML[0].style.backgroundColor = "white";
-  handleRound(1)
+const handleRestart = (event) => {
+  // highestAvailableRounds = 1;
+  // score.innerText = `Geese Fed:`;
+  // currentRound.innerHTML = `Current Round:`;
+  // roundTimer.innerHTML = `Round Timer: 00:00s`;
+
+  // levelButtonsHTML.forEach((buttonHTML) => {
+  //   buttonHTML.style.backgroundColor = "rgb(58, 58, 58)";
+  // });
+  // levelButtonsHTML[0].style.backgroundColor = "white";
+
+  // handleRound(event);
+  location.reload()
 };
 levelButtonsHTML[0].style.backgroundColor = "white";
 
 levelSelectButtons.forEach((levelButton) => {
   levelButton.addEventListener("click", handleRound);
 });
-// countDownTimer(10000)
-// console.log(roundTimer)
 restartButton.addEventListener("click", handleRestart);
